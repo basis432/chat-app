51972dc3da70e09b87d7e60f66fdb3a0499333bb
basis432 <captz999@gmail.com>
2025-06-15 18:40:40 +0700

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "GANTI_DENGAN_API_KEY_MU",
-  authDomain: "GANTI.firebaseapp.com",
-  databaseURL: "https://GANTI.firebaseio.com",
-  projectId: "GANTI",
-  storageBucket: "GANTI.appspot.com",
-  messagingSenderId: "GANTI",
-  appId: "GANTI"
+  apiKey: "AIzaSyBnkk02CHyiWG1ygWVV3g9O07jd5TFVNn0",
+  authDomain: "chatappgratis-d2649.firebaseapp.com",
+  databaseURL: "https://chatappgratis-d2649-default-rtdb.firebaseio.com",
+  projectId: "chatappgratis-d2649",
+  storageBucket: "chatappgratis-d2649.firebasestorage.app",
+  messagingSenderId: "425067394100",
+  appId: "1:425067394100:web:4b71a9c92cbbf636234661"
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
