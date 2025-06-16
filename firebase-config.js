87dc2e3f9b112b99cc40172be80e088ed8baa5eb
basis432 <captz999@gmail.com>
2025-06-16 16:02:00 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+// firebase-config.js
 
+// Konfigurasi Firebase milik kamu
 const firebaseConfig = {
   apiKey: "AIzaSyBnkk02CHyiWG1ygWVV3g9O07jd5TFVNn0",
   authDomain: "chatappgratis-d2649.firebaseapp.com",
@@ -13,7 +11,5 @@ const firebaseConfig = {
   appId: "1:425067394100:web:4b71a9c92cbbf636234661"
 };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getDatabase(app);
-export const storage = getStorage(app);
+// Inisialisasi Firebase
+firebase.initializeApp(firebaseConfig);
