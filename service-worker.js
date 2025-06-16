c8d9921f8f468cb472093008e210621cbef68606
basis432 <captz999@gmail.com>
2025-06-16 13:42:42 +0700

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,7 @@
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("chatapp").then((cache) => {
-      return cache.addAll(["./", "./index.html", "./style.css", "./app.js"]);
-    })
-  );
+self.addEventListener("install", e => {
+  console.log("Service worker installed");
 });
-self.addEventListener("fetch", (e) => {
-  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+
+self.addEventListener("fetch", e => {
+  // kosongkan dulu
 });
