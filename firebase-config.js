6ea3978a401dbe65d014956d069a5d4f59af9b92
basis432 <captz999@gmail.com>
2025-06-16 13:42:23 +0700

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: "ISI_DENGAN_API_KEY_KAMU",
-  authDomain: "xxxxx.firebaseapp.com",
-  projectId: "xxxxx",
-  databaseURL: "https://xxxxx.firebaseio.com",
-  storageBucket: "xxxxx.appspot.com",
-  messagingSenderId: "xxxxx",
-  appId: "xxxxx"
+  apiKey: "AIzaSyBnkk02CHyiWG1ygWVV3g9O07jd5TFVNn0",
+  authDomain: "chatappgratis-d2649.firebaseapp.com",
+  projectId: "chatappgratis-d2649",
+  databaseURL: "https://chatappgratis-d2649-default-rtdb.firebaseio.com",
+  storageBucket: "chatappgratis-d2649.firebasestorage.app",
+  messagingSenderId: "425067394100",
+  appId: "1:425067394100:web:4b71a9c92cbbf636234661"
 };
 
 firebase.initializeApp(firebaseConfig);
